6e43c5c7862b4e7d78bd7100288f8e2c8153e2fe
ankithaudupa <ankithaudupa7@gmail.com>
2022-03-08 12:48:49 -0500

    Initial Commit

---
+++ Project3/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Project3
+//
+//  Created by Ankitha Udupa on 3/8/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
